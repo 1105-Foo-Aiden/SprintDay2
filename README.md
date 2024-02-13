3a9bda788d2b6a3edde544a54abc286746423d74
Aiden Foo <aidenfoo154@gmail.com>
2024-02-13 15:42:14 -0800

    What I could get done

---
+++ README.md
@@ -0,0 +1,15 @@
+Aiden Foo
+Feb 13, 2024
+Challenge 5, enhanced RNG 
+
+### Thoughts:
+Well I thought it was going better then yesterday until 3PM today, I was using my POKeAPI to help with the logic for lacal storage and I thought it was alright. Which is when a couple of people told me that my entire way of doing local storage was wrong and that I missed an entire part of the instructionos, (the part about how we have to allow the user to pick not only the number of groups but also the size of them.)
+
+This was pretty good at wrecking my spirits then one of the people whowere looking at the code told me that I was spelling "Border" wrong this entire time and that was why the style wan't right. although it was funny, it realy weighed me down which sucks to admit but it ruined my flow a lot.
+
+Other then that the assignment seemed a lot more doable than yesterdays which I am happy about. Just everything else in the day ruined that for me.
+
+### So what works?
+Well adding the names works pretty well. And thats all for the things that work flawlessly. Removing the names has to be done from the bottom to the top otherwise it'll break and that's about it. Everything else either doesn't work or works 1/10 times. Most of the time was spent just trying to figure out how to remove things from local stoarge in the first place so there's a lot of things I have to work on I guess
+
+Just feels bad 😔
--- Scripts/app.js
+++ Scripts/app.js
@@ -2,39 +2,101 @@ let randBtn = document.getElementById("randBtn");
 let addNameBtn = document.getElementById("addNameBtn");
 let groupSize = document.getElementById("groupSizer");
 let groups = document.getElementById('groups')
+let groupsTxt = document.getElementById('groupsTxt');
+let amountOfGroups = document.getElementById('amountOfGroups')
+let h5
+let removeBtn
+let colDiv1
+let colDiv2
+let listRow
+let hr
+let h5count = document.querySelectorAll('h5')
+let nameList = []
 
 
-let nameList = [];
 
-randBtn.addEventListener("click", () => {});
+randBtn.addEventListener('click', () =>{
 
+        while(nameList.length){
+        const random = Math.floor(Math.random()*nameList.length)
+        const el = nameList.splice(random, 1)[0]
+        console.log(el)
+        }
+    
+    groupOne()
+    
+    groups.group1 = nameList.splice(0, groupSize.value)
+    groups.group2 = nameList.splice(groupSize.value)
+    console.log(groups.group1)
+    console.log(groups.group2)
+    groupsTxt.textContent = `Group 1: ${groups.group1.join(', ')}`
+    
+})
+
+
+
+
+let names = localStorage.getItem('Names')
+names = names ? JSON.parse(names) : []
+
+
+//function for creating name list
 const AddName = (name) => {
+    nameList.push(name)
+    console.log(nameList)
     totalNames.textContent = `Total Names: ${nameList.length}`
-    let h5 = document.createElement("h5");
-    let removeBtn = document.createElement("button");
-    let colDiv1 = document.createElement("div");
-    let colDiv2 = document.createElement("div");
-    let listRow = document.createElement("div");
-    let hr = document.createElement('hr')
-    listRow.classList = "row";
-
+    h5 = document.createElement("h5");
+    removeBtn = document.createElement("button");
+    colDiv1 = document.createElement("div");
+    colDiv2 = document.createElement("div");
+    listRow = document.createElement("div");
+    hr = document.createElement('hr')
+    listRow.classList = "row border-2 border-bottom border-solid border-black";
+    
     colDiv1.classList = "col";
     colDiv2.classList = "col";
     h5.textContent = name;
     removeBtn.classList = "btn btn-danger";
     removeBtn.textContent = "Remove";
     removeBtn.addEventListener('click', ()=>{
-        removeName()
-    })
+        removeName(name)
+    });
+    
     colDiv1.append(h5);
     colDiv2.append(removeBtn)
     listRow.append(colDiv1, colDiv2);
-    listDiv.append(listRow, hr);
+    listDiv.append(listRow);
  
 };
 
 addNameBtn.addEventListener("click", () => {
-  nameList.push(nameInput.value);
+  names = localStorage.getItem('Name')
+  names = names ? JSON.parse(names) : []
   AddName(nameInput.value)
   
+  localStorage.setItem('Name', JSON.stringify(nameList))
 });
+
+const removeName = (CurrentName) =>{
+    names = localStorage.getItem('Name')
+    names = names ? JSON.parse(names) : []
+    names = names.filter(name => name !== CurrentName)
+    listRow.remove(CurrentName)
+    localStorage.setItem('Name', JSON.stringify(names))
+    nameList = names
+    totalNames.textContent = names ? `Total Names: ${names.length}` : 'Total Names: 0'
+}
+
+const LSDetection = () =>{
+    
+    names = localStorage.getItem('Name')
+    names = names ? JSON.parse(names) : []
+    if(names.length !==0){
+        nameList = []
+        
+        names.forEach(name =>{AddName(name)})  
+    }
+    console.log(nameList)
+}
+
+LSDetection()
